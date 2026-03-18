bf9146310f6c4aa941b7b2ec253ed00575dd20ec
tpimorgan <tpimorgan@tpiglobal.network>
2026-03-18 18:13:50 +0900

    fix: next-pwa 완전 제거 - 캐시 문제 근본 해결

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
